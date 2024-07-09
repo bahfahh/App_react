0eb803dff43b7790eefee9bd54bd47662854388c
Po Wen <a87088@gmail.com>
2024-07-09 20:27:57 +0800

    Add DeleteActivity 1.05: <feat>(application):
    Changes:
    - Add DeleteActivity
    -Add ApplicationServiceExtensions
    -fix program.cs to use  ApplicationServiceExtensions;

--- API/Program.cs
+++ API/Program.cs
@@ -3,65 +3,30 @@ using Persistence;
 using Application;
 using Application.Core;
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-}
-);
-
-
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-
-});
-builder.Services.AddMediatR(cfg=>cfg.RegisterServicesFromAssemblies(typeof(ActivitiesList.Handler).Assembly));//cfg（configuration）
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
-
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseCors("CorsPolicy");
 app.UseAuthorization();
-
 app.MapControllers();
-
-
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
+
 try
 {
-
     System.Console.WriteLine("doing");
     var context = services.GetRequiredService<DataContext>();
     await context.Database.EnsureDeletedAsync();
-
     // Create a new database and apply migrations
     await context.Database.MigrateAsync();
-
-
     //context.Database.EnsureCreated();
     System.Console.WriteLine("before seed data");
     await Seed.SeedData(context);
@@ -69,9 +34,6 @@ try
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-
     logger.LogError(ex, "an error  occurred during migration");
-
 }
-
 app.Run();
---
+++ Application/DeleteActivity.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Persistence;
+namespace Application
+{
+    public class DeleteActivity
+    {
+        public class Command : IRequest<Unit>
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Command, Unit>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                System.Diagnostics.Debug.WriteLine($"Handling delete activity request for ID: {request.Id}");
+                System.Console.WriteLine(request.Id);
+                var activity = await _context.Activities.FindAsync(request.Id);
+                _context.Remove(activity);
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
