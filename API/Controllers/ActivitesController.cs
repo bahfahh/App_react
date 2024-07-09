ff87f99996bb264967b6db4bd95d0920642be56d
Po Wen <a87088@gmail.com>
2024-07-09 18:45:49 +0800

    feat(application): Implement activity deletion functionality
    Changes:
    - Add DeleteActivity handler in Application layer
    - Update ActivitiesController to support delete operation
    
    Reason:
    - Enhance CRUD operations by adding delete functionality for activities
    
    Details:
    - Create DeleteActivity.cs in Application layer to handle delete logic
    - Update ActivitiesController.cs to add delete endpoint
    - Adjust reactivity database schema to accommodate activity deletion
    
    Issue #N/A

--- API/Controllers/ActivitesController.cs
+++ API/Controllers/ActivitesController.cs
@@ -42,7 +42,12 @@ namespace API.Controllers
             activity.Id = id;
             return await Mediator.Send(new EditActivity.Command { ActivityDTO = activity });
         }
-        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteActivity(Guid id)
+        {
+               
+            return Ok(await Mediator.Send(new DeleteActivity.Command { Id = id }));
+        }   
     }
 
 }
